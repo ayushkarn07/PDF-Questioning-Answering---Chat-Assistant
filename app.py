6d4465748a2f87263e0c65d960e695fa5c0098ed
Ayush Karn <141517870+ayushkarn07@users.noreply.github.com>
2025-12-29 22:04:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 # ======================================
 # PAGE CONFIG
